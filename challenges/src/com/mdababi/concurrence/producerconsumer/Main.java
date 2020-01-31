cf2601250e000eb312fa9ff33fab18aa4a544287
mdababi <mdababi@mdababi-550-301nk>
2020-01-31 12:18:24 +0100

    reentrant lock

---
+++ challenges/src/com/mdababi/concurrence/producerconsumer/Main.java
@@ -0,0 +1,93 @@
+package com.mdababi.concurrence.producerconsumer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<String> buffer = new ArrayList<String>();
+		ReentrantLock bufferLock = new ReentrantLock();
+		myProducer producer = new myProducer(buffer, ThreadColor.ANSI_BLUE, bufferLock);
+		myConsumer consumer1 = new myConsumer(buffer, ThreadColor.ANSI_RED, bufferLock);
+		myConsumer consumer2 = new myConsumer(buffer, ThreadColor.ANSI_GREEN, bufferLock);
+
+		new Thread(producer).start();
+		new Thread(consumer1).start();
+		new Thread(consumer2).start();
+
+	}
+}
+
+class myProducer implements Runnable {
+	private List<String> buffer;
+	private String color;
+	private ReentrantLock bufferLock;
+
+	public myProducer(List<String> buffer, String color, ReentrantLock bufferLock) {
+		super();
+		this.buffer = buffer;
+		this.color = color;
+		this.bufferLock = bufferLock;
+	}
+
+	@Override
+	public void run() {
+		Random random = new Random();
+		String[] nums = { "1", "2", "3", "4", "5" };
+
+		for (String num : nums) {
+			try {
+				System.out.println(color + "Adding..." + num);
+				bufferLock.lock();
+				buffer.add(num);
+				bufferLock.unlock();
+				Thread.sleep(random.nextInt(2000));
+			} catch (InterruptedException e) {
+				System.out.println("Producer was interrupted");
+			}
+		}
+		System.out.println(color + "Adding EOF and exiting...");
+		bufferLock.lock();
+		buffer.add("EOF");
+		bufferLock.unlock();
+
+	}
+}
+
+class myConsumer implements Runnable {
+	private List<String> buffer;
+	private String color;
+	private ReentrantLock bufferLock;
+
+	public myConsumer(List<String> buffer, String color, ReentrantLock bufferLock) {
+		super();
+		this.buffer = buffer;
+		this.color = color;
+		this.bufferLock = bufferLock;
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			bufferLock.lock();
+			if (buffer.isEmpty()) {
+				bufferLock.unlock();
+				continue;
+			}
+			if (buffer.get(0).equals("EOF")) {
+				System.out.println(color + "exiting...");
+				bufferLock.unlock();
+				break;
+			} else {
+				System.out.println(color + "removed" + buffer.remove(0));
+			}
+			bufferLock.unlock();
+
+		}
+
+	}
+
+}
